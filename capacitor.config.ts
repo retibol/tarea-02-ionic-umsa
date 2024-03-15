4f5eec59e505cba71d6752a2cf2479c35e83b8cd
Franz Mamani <retibol.bolivia@gmail.com>
2024-03-15 11:35:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea02_ionic_umsa',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
